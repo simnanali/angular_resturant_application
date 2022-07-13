0a051b29eb412f874b2333539f0e291c6de44bf8
Simnan Ali <a.alnaeem@gmail.com>
2022-07-13 11:49:33 +0530

    Add files via upload
    Initial commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup
+
+  constructor(private formBuilder: FormBuilder, private _http: HttpClient, private router: Router) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: [''],
+      password: ['']
+    })
+  }
+
+  DoLogin() {
+    this._http.get<any>("http://localhost:3000/signup").subscribe(res => {
+      const user = res.find((a: any) => {
+        return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password
+      })
+
+      if (user) {
+        alert("Login success")
+        this.loginForm.reset();
+        this.router.navigate(['dashboard']);
+      }
+      else {
+        alert('enter proper email id and password');
+      }
+    },
+      err => {
+        alert("something went wrong from server side.");
+      }
+    )
+  }
+
+}
